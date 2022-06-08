564d6612192fe8417157b43c6d0d372c65fe2caf
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-06-09 00:27:56 +0300

    Добавляет функции для создания описания объявления

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+const TITLES = [
+  'Великолепный вид на город',
+  'Комфорт прежде всего',
+  'Цена заставит вас приятно удивиться',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'Современный ремонт и новая техника сделают пребывание здесь максимально комфортным',
+  'Что может быть прекраснее, чем насладиться утренней чашечкой кофе у открытого окна',
+  'Удобная парковка, лифт, сама квартира позволит с комфортом разместиться как минимум 3 постояльцам',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
 const checkNumberOnNegative = (number) =>  number < 0;
 
 const checkRange = (min, max) => min >= max;
@@ -30,6 +77,38 @@ const getCoordinates = (min, max, dec) => {
   return parseFloat(randomNumber.toFixed(dec));
 };
 
-getRandomNumber(2, 10);
-getCoordinates(1.5, 15.7, 2);
+const getRandomAvatarAddress = (min, max) => {
+  const avatarAddressIndex = getRandomNumber(min, max);
+  return (avatarAddressIndex >= 1 && avatarAddressIndex <= 9) ? `0${avatarAddressIndex}` : avatarAddressIndex;
+};
 
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const getArrayLength = (elements) => getRandomNumber(1, elements.length);
+
+const createAd = () => {
+  return {
+    author: {
+      avatar: `img/avatars/user${getRandomAvatarAddress(1, 10)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomNumber(0, 100000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(1, 100),
+      guests: getRandomNumber(1, 3),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: FEATURES.slice(1, getArrayLength(FEATURES)),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: PHOTOS.slice(1, getArrayLength(PHOTOS)),
+    },
+    location: {
+      lat: getCoordinates(35.65000, 35.70000, 5),
+      lng: getCoordinates(139.70000, 139.80000, 5),
+    },
+  };
+};
+
+console.log(createAd());
