1abe1bfa195c49d3a9aa472ecd810cfc63b1d810
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-06-27 19:14:02 +0300

    Выносит одинаковые переменные из функций по активации и деактивации формы

--- js/form.js
+++ js/form.js
@@ -1,14 +1,15 @@
+const form = document.querySelector('.ad-form');
+const formElements = form.querySelectorAll('.ad-form__element');
+const mapFiltersForm = document.querySelector('.map__filters');
+const mapFilters = mapFiltersForm.querySelectorAll('.map__filter');
+
 const makeFormInactive = () => {
-  const form = document.querySelector('.ad-form');
   form.classList.add('ad-form--disabled');
   form.querySelector('.ad-form-header').setAttribute('disabled', '');
-  const formElements = form.querySelectorAll('.ad-form__element');
   formElements.forEach((formElement) => {
     formElement.setAttribute('disabled', '');
   });
-  const mapFiltersForm = document.querySelector('.map__filters');
   mapFiltersForm.classList.add('map__filters--disabled');
-  const mapFilters = mapFiltersForm.querySelectorAll('.map__filter');
   mapFilters.forEach((mapFilter) => {
     mapFilter.setAttribute('disabled', '');
   });
@@ -16,16 +17,12 @@ const makeFormInactive = () => {
 };
 
 const makeFormActive = () => {
-  const form = document.querySelector('.ad-form');
   form.classList.remove('ad-form--disabled');
   form.querySelector('.ad-form-header').removeAttribute('disabled');
-  const formElements = form.querySelectorAll('.ad-form__element');
   formElements.forEach((formElement) => {
     formElement.removeAttribute('disabled');
   });
-  const mapFiltersForm = document.querySelector('.map__filters');
   mapFiltersForm.classList.remove('map__filters--disabled');
-  const mapFilters = mapFiltersForm.querySelectorAll('.map__filter');
   mapFilters.forEach((mapFilter) => {
     mapFilter.removeAttribute('disabled');
   });
