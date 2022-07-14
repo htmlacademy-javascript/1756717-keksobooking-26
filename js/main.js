551c861ffccf89e1d0855b95b6ea15ad7231f756
elistratovamaria <Elistratova.Mari.22@yandex.ru>
2022-07-15 01:47:54 +0300

    Добавляет функции фильтрации

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 import { makeFormInactive, setFormSubmit, clearForm } from './form.js';
-import { activateMap, createMapMarker} from './map.js';
+import { activateMap, clearMap, createMapMarker } from './map.js';
 import { getData } from './load.js';
-import { showErrorLoadMessage } from './util.js';
+import { showErrorLoadMessage, debounce } from './util.js';
+import { makeFilterActive, onFilterChange, showFilteredAds } from './filter.js';
 
 const MAX_SIMILAR_ADS_AMOUNT = 10;
+const RERENDER_DELAY = 200;
 
 makeFormInactive();
 
@@ -11,10 +13,13 @@ activateMap();
 
 getData(
   (data) => {
-    const ads = data.slice(0, MAX_SIMILAR_ADS_AMOUNT);
-    ads.forEach((ad) => {
-      createMapMarker(ad);
-    });
+    const ads = data;
+    makeFilterActive();
+    showFilteredAds(ads, MAX_SIMILAR_ADS_AMOUNT, createMapMarker);
+    onFilterChange(debounce(() => {
+      clearMap();
+      showFilteredAds(ads, MAX_SIMILAR_ADS_AMOUNT, createMapMarker);
+    }, RERENDER_DELAY));
   },
   () => showErrorLoadMessage('Не удалось получить данные объявлений')
 );
